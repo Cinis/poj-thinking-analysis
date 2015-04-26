733973a9a57f492559e058de6cc0ea93f57225b0
Cinis <cy1215181830@live.com>
2015-04-26 17:14:57 +0800

    Create poj1007.cpp

---
+++ poj1007.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cstdio>
+
+using namespace std;
+
+typedef struct
+{
+    string str;//存储DNA序列
+    int num;//存储每个DNA序列的逆序数
+}DNA;
+DNA d[100];
+
+bool cmp(DNA a,DNA b)//比较函数，结合main函数里面的stable_sort对数组进行排序
+{
+    return a.num<b.num;
+}
+
+int main()
+{
+    int m,n;
+    cin>>m>>n;//输入m，n m字符串的长度，n有几个试用例
+    int i=0;
+    for(i=0;i<n;i++)
+    {
+        cin>>d[i].str;
+        d[i].num=0;
+        for(int k=1;k<m;k++)
+        {
+            for(int j=0;j<k;j++)
+            {
+                if(d[i].str[k]<d[i].str[j])
+                    d[i].num++;
+            }
+        }
+    }
+
+    stable_sort(d,d+n,cmp);
+    
+    for( i=0;i<n;i++)
+    {
+        cout<<d[i].str<<endl;
+    }
+
+    return 0;
+}
